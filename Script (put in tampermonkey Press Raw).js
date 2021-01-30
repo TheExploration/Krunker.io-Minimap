90d6f49baf3a84e437f6e83202c4513e035b9494
Squidable <zhangphil@outlook.com>
2021-01-30 13:27:45 -0800

    Create Script (put in tampermonkey Press Raw).js

---
+++ Script (put in tampermonkey Press Raw).js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Basic Krunker Minimap
+//@author        I have no idea
+// @match        *://krunker.io/*
+// @grant        none
+// ==/UserScript==
+
+Object.defineProperty(Object.prototype, "drawMinimap", { writeable: false, value: true })
